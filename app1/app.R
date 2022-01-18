c0c52c732ec78eb583e1681d4f8c8ba73f653373
seymakalay <seymakalay@hotmail.com>
2022-01-18 19:22:32 +0100

    3 rd commit

--- app1/app.R
+++
@@ -1,6 +0,0 @@
-ui <- fluidPage(p("App 1 content"), 
-sliderInput("obs", "Number of observations:",
-    min = 0, max = 1000, value = 500
-  ))
-server <- function(input, output, session) {}
-shinyApp(ui, server)
